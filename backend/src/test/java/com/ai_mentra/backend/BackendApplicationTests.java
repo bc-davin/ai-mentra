32cce7b6563db4876738fe923cbb718c0935c9b0
Davin <71451687+bc-davin@users.noreply.github.com>
2025-11-16 08:16:30 -0700

    initial

---
+++ backend/src/test/java/com/ai_mentra/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_mentra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
